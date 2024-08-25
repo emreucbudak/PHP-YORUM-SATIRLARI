854d59d51981fc4ed7a6975ac639c11462912c9e
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 21:28:38 +0300

    Add files via upload

---
+++ main (4).php
@@ -0,0 +1,8 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniveristesi
+
+*******************************************************************************/
+// Yorum Satırlarını ezberlemeye gerek yok çoğu idede artık ctrl+ö tuşlarıyla oluyor
+?>
